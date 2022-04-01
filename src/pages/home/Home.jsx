eaa10d090a5adc7b423eb247b2ea09bb24193a18
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-04-01 13:35:21 +0430

    TrendingSection fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import HeroSection from "../../components/ui/HeroSection";
 import LiveAuction from "../../components/ui/live-auction/LiveAuction";
 import SellerSection from "../../components/ui/seller-section/SellerSection";
-
+import TrendingSection from "../../components/ui/trending-section/TrendingSection";
 const Home = () => {
   return (
     <>
       <HeroSection />
       <LiveAuction />
       <SellerSection />
+      <TrendingSection />
     </>
   );
 };
