443dea6b67934c9917617d7b05c9decba43a9290
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-04-29 19:04:51 +0430

    (hotfiexd) fixed details pages

--- src/pages/nftDetails/NftDetails.jsx
+++ src/pages/nftDetails/NftDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
 
 import CommonSection from "../../components/ui/common-section/CommonSection";
@@ -19,27 +19,62 @@ const NftDetails = () => {
         <Container>
           <Row>
             <Col lg={"6"} md="6">
-              <img src={singleNft.imgUrl} alt={singleNft.title} className="w-100 single__nft-img"  />
+              <img
+                src={singleNft.imgUrl}
+                alt={singleNft.title}
+                className="w-100 single__nft-img"
+              />
             </Col>
             <Col lg="6" md="6">
               <div className="single__nft__contnet">
                 <h2>{singleNft.title}</h2>
 
-                <div className="d-flex aligin-item-center justify-content-between">
-                  <div className="d-flex aligin-item-center gap-4">
-                    <span><i class="ri-eye-line"></i> 234</span>
-                    <span><i class="ri-heart-line"></i> 123</span>
+                <div className="d-flex aligin-item-center justify-content-between mt-4 mb-4">
+                  <div className="d-flex aligin-item-center gap-4 single__nft-seen">
+                    <span>
+                      <i class="ri-eye-line"></i> 234
+                    </span>
+                    <span>
+                      <i class="ri-heart-line"></i> 123
+                    </span>
                   </div>
 
-                  <div className="d-flex aligin-item-center gap-4">
-                    <span><i class="ri-send-plane-line"></i></span>
-                    <span><i class="ri-more-2-line"></i></span>
+                  <div className="d-flex aligin-item-center gap-2 single__nft-more">
+                    <span>
+                      <i class="ri-send-plane-line"></i>
+                    </span>
+                    <span>
+                      <i class="ri-more-2-line"></i>
+                    </span>
                   </div>
                 </div>
+                <div className="nft__creator d-flex gap-3 align-items-center">
+                  <div className="creator__img">
+                    <img
+                      src={singleNft.creatorImg}
+                      alt={singleNft.title}
+                      className="w-100"
+                    />
+                  </div>
+
+                  <div className="creator__detail">
+                    <p>Created By</p>
+                    <h6>{singleNft.creator}</h6>
+                  </div>
+                </div>
+
+                <p className="my-4">{singleNft.desc}</p>
+
+                <button className="singleNft-btn  d-flex align-items-center gap-2 w-100">
+                  <i class="ri-shopping-bag-line"></i>
+                  <Link to="/wallet">Place a Bid</Link>
+                </button>
               </div>
             </Col>
           </Row>
         </Container>
+
+        <LiveAuction />
       </section>
     </>
   );
